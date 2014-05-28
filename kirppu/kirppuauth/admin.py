319130a514d21550a5b59bc47172c0b7d8efd2cd
Arttu Ylä-Outinen <yleisnimi@gmail.com>
2014-05-28 18:17:33 +0300

    Fix adding users through the admin interface.

--- kirppu/kirppuauth/admin.py
+++ kirppu/kirppuauth/admin.py
@@ -1,7 +1,35 @@
 from django.contrib.auth.admin import UserAdmin
-from kirppu.kirppuauth.models import User
+from django.contrib.auth.forms import (
+    UserCreationForm,
+    UserChangeForm,
+)
+from django.forms import forms
 from django.contrib import admin
+
+from kirppu.kirppuauth.models import User
 
 __author__ = 'codez'
 
-admin.site.register(User, UserAdmin)
+class KirppuUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        try:
+            User.objects.get(username=username)
+        except User.DoesNotExist:
+            return username
+        raise forms.ValidationError(
+            self.error_messages['duplicate_username']
+        )
+
+class KirppuUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = User
+
+class KirppuUserAdmin(UserAdmin):
+    form = KirppuUserChangeForm
+    add_form = KirppuUserCreationForm
+
+admin.site.register(User, KirppuUserAdmin)
