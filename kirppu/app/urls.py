6de96ef01d24d01e704b35864d5a687f60063f7e
Ari Koivula <ari@koivu.la>
2014-05-28 20:41:23 +0300

    Add line-ends to terminal url regexps.
    - This ensures that parameters are not ignored if they do not match the
      regexp.
    
    Conflicts:
    	kirppu/app/urls.py

--- kirppu/app/urls.py
+++ kirppu/app/urls.py
@@ -3,9 +3,9 @@ from django.conf.urls import patterns, url
 __author__ = 'jyrkila'
 
 urlpatterns = patterns('kirppu.app.views',
-    url(r'^page/(?P<sid>\d+)/(?P<eid>\d+)', 'get_items', name='page'),
-    url(r'^code/(?P<iid>\w+?)\.(?P<ext>\w+)', 'get_item_image', name='image'),
-    url(r'^commands/(?P<eid>\d+)', 'get_commands', name='commands'),
-    url(r'^command/(?P<iid>\w+?)\.(?P<ext>\w+)', 'get_command_image', name='command_image'),
-    url(r'^checkout/(?P<eid>\d+)', 'checkout_view'),
+    url(r'^page/(?P<sid>\d+)/(?P<eid>\d+)$', 'get_items', name='page'),
+    url(r'^code/(?P<iid>\w+?)\.(?P<ext>\w+)$', 'get_item_image', name='image'),
+    url(r'^commands/(?P<eid>\d+)$', 'get_commands', name='commands'),
+    url(r'^command/(?P<iid>\w+?)\.(?P<ext>\w+)$', 'get_command_image', name='command_image'),
+    url(r'^checkout/(?P<eid>\d+)$', 'checkout_view'),
 )
