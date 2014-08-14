5fe794ec531a59db5082a0eafafc0e3151e9ba77
Arttu Ylä-Outinen <yleisnimi@gmail.com>
2014-08-14 12:09:29 +0300

    Remove dead code.

--- kirppu/app/forms.py
+++ kirppu/app/forms.py
@@ -1,35 +1,6 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
 
-from kirppu.app.models import Vendor, Clerk
-from kirppu.kirppuauth.models import User
-from kirppu.kirppuauth.admin import KirppuUserCreationForm
-
-class VendorAuthenticationForm(AuthenticationForm):
-
-    def clean(self):
-        ret = super(VendorAuthenticationForm, self).clean()
-
-        # Check that the user is registered as a vendor.
-        user = self.get_user()
-        if user and not user.is_vendor():
-            raise forms.ValidationError(
-                self.error_messages['invalid_login'],
-                code='invalid_login',
-                params={'username': self.username_field.verbose_name},
-            )
-
-        return ret
-
-
-class VendorCreationForm(KirppuUserCreationForm):
-
-    def save(self):
-        user = super(VendorCreationForm, self).save(commit=False)
-        user.save()
-        Vendor.objects.create(user=user)
-        return user
-
+from kirppu.app.models import Clerk
 
 class ClerkGenerationForm(forms.ModelForm):
     count = forms.IntegerField(
