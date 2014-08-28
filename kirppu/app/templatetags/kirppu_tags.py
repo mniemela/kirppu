c7accaa5431254face86d9921855e353370a8a76
Ari Koivula <ari@koivu.la>
2014-08-28 21:33:42 +0300

    Cache barcode dataurls for faster pageloads.
    - Significantly reduces the time it takes to reload a page with hundreds of
      barcodes.

--- kirppu/app/templatetags/kirppu_tags.py
+++ kirppu/app/templatetags/kirppu_tags.py
@@ -1,16 +1,38 @@
 from kirppu.app.utils import PixelWriter
 import barcode
-
-
 from django import template
 register = template.Library()
 import base64
 from cStringIO import StringIO
+from django.utils.functional import memoize
+from collections import OrderedDict
 
 
-@register.simple_tag
-def barcode_dataurl(code, ext):
-    if not code or code == 'X':
+class FifoDict(OrderedDict):
+    def __init__(self, *args, **kwargs):
+        self.limit = kwargs.pop("limit", None)
+        OrderedDict.__init__(self, *args, **kwargs)
+        self._remove_oldest()
+
+    def __setitem__(self, key, value):
+        OrderedDict.__setitem__(self, key, value)
+        self._remove_oldest()
+
+    def _remove_oldest(self):
+        if not self.limit:
+            return
+
+        while len(self) > self.limit:
+            self.popitem(last=False)
+
+
+# Limit the size of the dict to a reasonable number so that we don't have
+# millions of dataurls cached.
+barcode_dataurl_cache = FifoDict(limit=50000)
+
+
+def generate_dataurl(code, ext):
+    if not code:
         return ''
 
     writer = PixelWriter(format=ext)
@@ -27,3 +49,10 @@ def barcode_dataurl(code, ext):
     return dataurl_format.format(
             mimetype=mimetype,
             base64_data=base64.encodestring(data))
+get_dataurl = memoize(generate_dataurl, barcode_dataurl_cache, 2)
+
+
+@register.simple_tag
+def barcode_dataurl(code, ext):
+    return get_dataurl(code, ext)
+
