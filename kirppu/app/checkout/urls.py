98ab2a2ac0279f504195e49d55ff7be817592a75
Arttu Ylä-Outinen <yleisnimi@gmail.com>
2014-08-29 19:38:56 +0300

    Fix access to checkout API.
    Prevent access to checkout API urls when checkout is not activated by
    not creating urlpatterns.

--- kirppu/app/checkout/urls.py
+++ kirppu/app/checkout/urls.py
@@ -1,12 +1,19 @@
+from django.conf import settings
 from django.conf.urls import url, patterns
 from .api import AJAX_FUNCTIONS
 
 __author__ = 'jyrkila'
 
-_urls = [url('^checkout.js$', 'checkout_js', name='checkout_js')]
-_urls.extend([
-    url(func.url, func.name, name=func.view_name)
-    for func in AJAX_FUNCTIONS.itervalues()
-])
+if settings.KIRPPU_CHECKOUT_ACTIVE:
+    # Only activate API when checkout is active.
+
+    _urls = [url('^checkout.js$', 'checkout_js', name='checkout_js')]
+    _urls.extend([
+        url(func.url, func.name, name=func.view_name)
+        for func in AJAX_FUNCTIONS.itervalues()
+    ])
+
+else:
+    _urls = []
 
 urlpatterns = patterns('kirppu.app.checkout.api', *_urls)
